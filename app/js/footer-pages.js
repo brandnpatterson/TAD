d94e5c873184850258816f2ecd04ed39835fa5b7
Brandon Patterson <brandonnpatterson@gmail.com>
2016-08-22 18:14:28 -0500

    Added jqyery to the nav to close mobile nav/move viewport when item is clicked

--- app/js/footer-pages.js
+++ app/js/footer-pages.js
@@ -1,84 +1,60 @@
 $(document).ready(function(){
-  $('#about, #carpentry, #components, #contact, #cutlery, #gardening, #hygiene, #mechanic, #n-abler, #sports-gear, #utencils').hide();
 
-  $('#nav-about').click(function about(){
-    $('#about').siblings().hide();
-    $('#about').show();
-    $('.navbar-toggle').click();
-    window.scrollTo(115, 115);
-  });
-
-  $('#nav-carpentry').click(function carpentry(){
+  $('#footer-carpentry').click(function carpentry(){
     $('#carpentry').siblings().hide();
     $('#carpentry').show();
-    $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-components').click(function(){
+  $('#footer-components').click(function(){
     $('#components').siblings().hide();
     $('#components').show();
-    $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-contact').click(function(){
-    $('#contact').siblings().hide();
-    $('#contact').show();
-    $('.navbar-toggle').click();
-    window.scrollTo( 115, 115);
-  });
-
-  $('#nav-cutlery').click(function(){
+  $('#footer-cutlery').click(function(){
     $('#cutlery').siblings().hide();
     $('#cutlery').show();
     $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-gardening').click(function(){
+  $('#footer-gardening').click(function(){
     $('#gardening').siblings().hide();
     $('#gardening').show();
     $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-home').click(function(){
-    $('#home').siblings().hide();
-    $('#home').show();
-    $('.navbar-toggle').click();
-    window.scrollTo( 115, 115);
-  });
-
-  $('#nav-hygiene').click(function(){
+  $('#footer-hygiene').click(function(){
     $('#hygiene').siblings().hide();
     $('#hygiene').show();
     $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-mechanic').click(function(){
+  $('#footer-mechanic').click(function(){
     $('#mechanic').siblings().hide();
     $('#mechanic').show();
     $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-n-abler').click(function(){
+  $('#footer-n-abler, #footer-n-abler_V').click(function(){
     $('#n-abler').siblings().hide();
     $('#n-abler').show();
     $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-sports-gear').click(function(){
+  $('#footer-sports-gear').click(function(){
     $('#sports-gear').siblings().hide();
     $('#sports-gear').show();
     $('.navbar-toggle').click();
     window.scrollTo( 115, 115);
   });
 
-  $('#nav-utencils').click(function(){
+  $('#footer-utencils').click(function(){
     $('#utencils').siblings().hide();
     $('#utencils').show();
     $('.navbar-toggle').click();
