34e877af4b685166bca73187c900e74c1a59f781
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-14 11:08:27 -0600

    removed media queries. wrote closeNavBar onclick

---
+++ app/js/close-navbar.js
@@ -0,0 +1,23 @@
+var CloseNavbar = (function(){
+
+  // cacheDOM
+  var $dropDownMenu = $('.dropdown-menu');
+  var $navbarToggle = $('.navbar-toggle');
+  var $navRoot = $('.nav-root');
+
+  // bind events
+  $navRoot.click(whenDropHidden);
+  $dropDownMenu.click(whenDropVisible);
+
+  function whenDropHidden() {
+    if ($navbarToggle.is(':visible') && $dropDownMenu.is(':hidden')) {
+      $navbarToggle.trigger('click');
+    }
+  }
+
+  function whenDropVisible() {
+    if ($dropDownMenu.is(':visible')) {
+      $navbarToggle.trigger('click');
+    }
+  }
+})();
