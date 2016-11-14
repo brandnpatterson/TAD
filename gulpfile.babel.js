e7280604b48c5966f4e865e77a05e33df952dd39
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-14 11:38:33 -0600

    gulpfile ;;;

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,41 +1,41 @@
-import gulp       from 'gulp'
-import concat     from 'gulp-concat'
-import del        from 'del'
-import eslint     from 'gulp-eslint'
-import load       from 'gulp-load-plugins'
-import prefix     from 'gulp-autoprefixer'
-import pug        from 'gulp-pug'
-import rename     from 'gulp-rename'
-import sass       from 'gulp-sass'
-import sourcemaps from 'gulp-sourcemaps'
-import sync       from 'browser-sync'
+import gulp       from 'gulp';
+import concat     from 'gulp-concat';
+import del        from 'del';
+import eslint     from 'gulp-eslint';
+import load       from 'gulp-load-plugins';
+import prefix     from 'gulp-autoprefixer';
+import pug        from 'gulp-pug';
+import rename     from 'gulp-rename';
+import sass       from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import sync       from 'browser-sync';
 
-const $ = load()
-const reload = sync.reload
+const $ = load();
+const reload = sync.reload;
 
-gulp.task('build', ['index', 'html', 'pug-pretty', 'lint'])
+gulp.task('build', ['index', 'html', 'pug-pretty', 'lint']);
 
-gulp.task('clean', del.bind(null, ['index.html', 'style.css', 'app/assets/views/*', 'dist/views/*', 'dist/index.min.js'], {read: false}))
+gulp.task('clean', del.bind(null, ['index.html', 'style.css', 'app/assets/views/*', 'dist/views/*', 'dist/*.min.js'], {read: false}));
 
 gulp.task('default', ['build', 'watch'], () => {
-  gulp.start('serve')
-})
+  gulp.start('serve');
+});
 
 gulp.task('html', () => {
   return gulp.src(['!app/pug/head.pug', 'app/pug/*.pug'])
     .pipe(sourcemaps.init())
     .pipe(pug())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/views'))
-})
-
+    .pipe(gulp.dest('./dist/views'));
+});
+;
 gulp.task('index', ['scripts', 'styles'], () => {
   return gulp.src('app/pug/index.pug')
     .pipe(sourcemaps.init())
     .pipe(pug())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'))
-})
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('images', () => {
   return gulp.src('app/assets/images/**/*')
@@ -44,25 +44,25 @@ gulp.task('images', () => {
       interlaced: true,
       svgoPlugins: [{cleanupIDs: false}]
     })))
-    .pipe(gulp.dest('dist/images'))
-})
+    .pipe(gulp.dest('dist/images'));
+});
 
 gulp.task('lint', () => {
   return gulp.src(['*/**/*.js', '!node_modules/*', '!dist/includes/*'])
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-})
+    .pipe(eslint.failAfterError());
+});
 
 gulp.task('pug-pretty', () => {
   return gulp.src(['!app/pug/head.pug', 'app/pug/*.pug'])
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest('app/assets/views'))
-})
+    .pipe(gulp.dest('app/assets/views'));
+});
 
-gulp.task('rebuild', ['clean', 'default'])
+gulp.task('rebuild', ['clean', 'default']);
 
 gulp.task('scripts', () => {
   return gulp.src('app/js/*.js')
@@ -72,8 +72,8 @@ gulp.task('scripts', () => {
     .pipe($.uglify())
     .pipe($.rename({suffix: '.min'}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'))
-})
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('serve', () => {
   sync({
@@ -82,8 +82,8 @@ gulp.task('serve', () => {
       baseDir: './'
     },
     port: 8888
-  })
-})
+  });
+});
 
 gulp.task('styles', () => {
   gulp.src('app/css/style.scss')
@@ -91,11 +91,11 @@ gulp.task('styles', () => {
   .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
   .pipe(prefix('last 2 versions'))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./'))
-})
+  .pipe(gulp.dest('./'));
+});
 
 gulp.task('watch', () => {
   gulp.watch('app/pug/*', ['index', 'html', 'pug-pretty', reload])
   gulp.watch('app/css/**/*', ['styles', reload])
-  gulp.watch('app/js/*', ['scripts', reload])
-})
+  gulp.watch('app/js/*', ['scripts', reload]);
+});
